7e34640e93f358d0ba9ffa8498689cebd658e9c6
Joseph Lord <joseph@human-friendly.com>
2015-01-31 23:56:17 +0000

    Rearrange into separate functions and types rather than inner functions.

--- SwiftResistanceSwift/SwiftResistanceSwift/Array.swift
+++ SwiftResistanceSwift/SwiftResistanceSwift/Array.swift
@@ -8,65 +8,66 @@
 
 import Foundation
 
-func pixelArrayTest(iterations: Int) -> Float {
-    struct Pixel {
-        var red: Byte
-        var green: Byte
-        var blue: Byte
-        var alpha: Byte
-    }
-    
-    struct RenderBuffer {
-        var pixels: [Pixel]
-        var width: Int
-        var height: Int
-        
-        init(width: Int, height: Int) {
-            assert(width > 0)
-            assert(height > 0)
-            
-            let pixel = Pixel(red: 0, green: 0, blue: 0, alpha: 0xFF)
-            pixels = [Pixel](count: width * height, repeatedValue: pixel)
-            
-            self.width = width
-            self.height = height
-        }
-    }
-    
-    func RenderGradient(inout buffer: RenderBuffer, offsetX: Int, offsetY: Int)
-    {
-        // I truly hope you have turned down the number of iterations or you have picked
-        // up a new build of Swift where this is not dog slow with -Onone.
-        var offset = 0
-        for (var y = 0, height = buffer.height; y < height; ++y) {
-            for (var x = 0, width = buffer.width; x < width; ++x) {
-                let pixel = Pixel(
-                    red: 0,
-                    green: Byte((y + offsetY) & 0xFF),
-                    blue: Byte((x + offsetX) & 0xFF),
-                    alpha: 0xFF)
-                buffer.pixels[offset] = pixel;
-                ++offset;
-            }
-        }
+struct Pixel {
+    var red: Byte
+    var green: Byte
+    var blue: Byte
+    var alpha: Byte
+}
 
-        // Turn this code on for at least some sanity back to your debug builds. It's still
-        // going to be slow, but at compared to the code above, it's going to feel glorious.
-//        buffer.pixels.withUnsafeMutableBufferPointer { (inout p: UnsafeMutableBufferPointer<Pixel>) -> () in
-//            var offset = 0
-//            for (var y = 0, height = buffer.height; y < height; ++y) {
-//                for (var x = 0, width = buffer.width; x < width; ++x) {
-//                    let pixel = Pixel(
-//                        red: 0,
-//                        green: Byte((y + offsetY) & 0xFF),
-//                        blue: Byte((x + offsetX) & 0xFF),
-//                        alpha: 0xFF)
-//                    p[offset] = pixel
-//                    ++offset;
-//                }
-//            }
-//        }
+struct RenderBuffer {
+    var pixels: [Pixel]
+    var width: Int
+    var height: Int
+    
+    init(width: Int, height: Int) {
+        assert(width > 0)
+        assert(height > 0)
+        
+        let pixel = Pixel(red: 0, green: 0, blue: 0, alpha: 0xFF)
+        pixels = [Pixel](count: width * height, repeatedValue: pixel)
+        
+        self.width = width
+        self.height = height
     }
+}
+
+func RenderGradient(inout buffer: RenderBuffer, offsetX: Int, offsetY: Int)
+{
+    // I truly hope you have turned down the number of iterations or you have picked
+    // up a new build of Swift where this is not dog slow with -Onone.
+    var offset = 0
+    for (var y = 0, height = buffer.height; y < height; ++y) {
+        for (var x = 0, width = buffer.width; x < width; ++x) {
+            let pixel = Pixel(
+                red: 0,
+                green: Byte((y + offsetY) & 0xFF),
+                blue: Byte((x + offsetX) & 0xFF),
+                alpha: 0xFF)
+            buffer.pixels[offset] = pixel;
+            ++offset;
+        }
+    }
+    
+    // Turn this code on for at least some sanity back to your debug builds. It's still
+    // going to be slow, but at compared to the code above, it's going to feel glorious.
+    //        buffer.pixels.withUnsafeMutableBufferPointer { (inout p: UnsafeMutableBufferPointer<Pixel>) -> () in
+    //            var offset = 0
+    //            for (var y = 0, height = buffer.height; y < height; ++y) {
+    //                for (var x = 0, width = buffer.width; x < width; ++x) {
+    //                    let pixel = Pixel(
+    //                        red: 0,
+    //                        green: Byte((y + offsetY) & 0xFF),
+    //                        blue: Byte((x + offsetX) & 0xFF),
+    //                        alpha: 0xFF)
+    //                    p[offset] = pixel
+    //                    ++offset;
+    //                }
+    //            }
+    //        }
+}
+
+func pixelArrayTest(iterations: Int) -> Float {
     
     let start = mach_absolute_time()
     
